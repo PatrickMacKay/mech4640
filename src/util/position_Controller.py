fb78e81fc25b4e4e0f403254d44cca6e1a5eee10
Kory Melchior <kory.melchior@dal.ca>
2023-03-14 16:00:05 -0300

    Added first few functions not much working yet

--- src/util/position_Controller.py
+++ src/util/position_Controller.py
@@ -1,9 +1,9 @@
 import numpy as np
-from timer import timer #needs to be adjusted for whatever james filename is
+import timer
 
 
 class PoistionController:
-    def __init__(self, p = 1, i = 1, d = 1, xtarget = 0, ytarget = y):
+    def __init__(self, p = 1, i = 1, d = 1, xtarget = 0, ytarget = 0):
         self.p = p
         self.i = i
         self.d = d
@@ -13,6 +13,20 @@ class PoistionController:
 
         self.xmeasure = 0
         self.ymeasure = 0
+        self.x_array = []
+        self.y_array = []
+
+    def update_pose(self):
+        # add current calculation to array for plotting
+        self.x_array.append(self.xmeasure)
+        self.y_array.append(self.ymeasure)
+        # calculate position delta based on encoder values
+        x_delta
+        y_delta
+        #update measured position
+        self.xmeasure = self.xmeasure + x_delta
+        self.ymeasure = self.ymeasure + y_delta
+
 
 
 
