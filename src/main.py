7953a1228850df8d16d807af2112ab61fcd31727
James Graham <james.joe.graham@gmail.com>
2023-03-14 15:42:29 -0300

    moved config out of src

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
-from util.timer import Timer
 import json
+import os
 
+from util.timer import Timer
+
+CTRL_PARAMS = "config/ctrl_params.json"
+WAYPOINTS_FILE = "config/waypoints.json"
 
 # Entry point
 if __name__ == "__main__":
     init_timer = Timer()
     print("Program Starting")
+
     # Import controller config
+    dirname = os.path.dirname(__file__).join("../")
+    dirname = os.path.join(dirname, "../")
+
+    wp_path = os.path.join(dirname)
+    print(dirname)
     
     # Print initialization time
     print("Initialization completed in %1.2f seconds" %init_timer.elapsed())
