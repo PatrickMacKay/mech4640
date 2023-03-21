9a571ffe810a4a3b5e7872c34f05ee9d04390a6d
Kory Melchior <kory.melchior@dal.ca>
2023-03-21 15:39:06 -0300

    Added calculations for vel and angular vel based on current and target position

--- src/util/position_Controller.py
+++ src/util/position_Controller.py
@@ -12,6 +12,8 @@ class PoistionController:
         self.i = i
         self.d = d
 
+        self.vel_limit = 1 # m/s
+        self.angvel_limit = 0.5 # m/s
         self.xtarget = xtarget
         self.ytarget = ytarget
         self.thtarget = thtarget
@@ -20,10 +22,10 @@ class PoistionController:
         self.vc = velocityController()
 
         # set PID values
-        self.PIDx = PIDController(p, i, d, 1)
-        self.PIDy = PIDController(p, i, d, 1)
-        self.PIDx.setpoint = self.xtarget
-        self.PIDy.setpoint = self.ytarget
+        self.PIDvel = PIDController(p, i, d, 1)
+        self.PIDth = PIDController(p, i, d, 1)
+        self.PIDvel.setpoint = np.sqrt(np.square(self.xtarget)+np.square(self.ytarget))
+        self.PIDth.setpoint = self.ytarget
 
         self.xmeasure = 0
         self.ymeasure = 0
@@ -37,6 +39,7 @@ class PoistionController:
         l_sig, r_sig, l_time, r_time, l_vel, r_vel, l_enc, r_enc = self.vc.update()
         # add current calculation to array for plotting
         self.x_array.append(self.xmeasure)
+
         self.y_array.append(self.ymeasure)
         self.th_array.append(self.thmeasure)
         # calculate position delta based on encoder values
@@ -49,12 +52,42 @@ class PoistionController:
         self.ymeasure = self.ymeasure + y_delta
 
 
-    def update_vel(self):
+    def update_vel(self, dt):
         # based on current and target position, update velocity
-        
-
         self.PIDx.state = self.xmeasure
         self.PIDy.state = self.ymeasure
+        vel = np.sqrt(np.square(self.xtarget-self.xmeasure)+np.square(self.ytarget-self.ymeasure))
+        th_to_target = np.arctan2((self.ytarget-self.ymeasure)/(self.xtarget-self.ymeasure))
 
+        ang_vel = max(self.angdiff(self.thmeasure, th_to_target), self.angvel_limit)
+
+
+        self.PIDx.update(dt)
+        self.PIDy.update(dt)
+
+
+    def angdiff(self, th1, th2):
+        # taking the difference
+        diff = th1 - th2
+
+        # checking if result is greater than 360 and reducing to within 1 rotation
+        if abs(diff) > 360:
+            r = diff // 360
+            diff = diff - r * 360
+
+        # if difference is negative, change to postive rotation to make next step easier
+        if diff < 0:
+            diff = 360 + diff
+
+        # checking if answer is within range pi or -pi
+        if 0 < diff < 180:
+            pass
+        elif 180 < diff < 360:
+            diff = -(360-diff)
+
+        # transform to radians for answer
+        diff_rad = diff * np.pi/180
+
+        return diff_rad
 
 
