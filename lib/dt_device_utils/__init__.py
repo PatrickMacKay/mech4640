ecb4a74dc0621f5613f1ba21a6448e9384db0d43
James Graham <james.joe.graham@gmail.com>
2023-03-21 16:20:34 -0300

    moved lib into src

--- lib/dt_device_utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .constants import \
-    DeviceHardwareBrand,\
-    DeviceHardwareModel
-
-from .device import \
-    get_device_id,\
-    get_device_hostname,\
-    get_device_hardware_brand
--- lib/dt_robot_utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .constants import RobotType, RobotConfiguration
-from .robot import get_robot_name, get_robot_type, get_robot_configuration
