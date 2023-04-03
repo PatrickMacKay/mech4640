ef173fb2c1d5c36e023129789168db6a3767be55
Kory Melchior <kory28195@gmail.com>
2023-04-02 21:45:44 -0300

    changed cam

--- src/cam_capture.py
+++ src/cam_capture.py
@@ -1,15 +1,33 @@
 import picamera
+import os
 from datetime import datetime
 import time
-camera = picamera.PiCamera()
-camera.resolution = (2592,1944)
-camera.annotate_background = picamera.Color('black')
 
-for c in range(0,25):
-    countDown = 3
-    camera.start_preview()
-    input("press enter to continue")
-    camera.annotate_test = ""
-    camera.capture("CalibrationPicyure{0:02d}.jpg".format(c))
-camera.stop_preview()
-camera.close()
+
+class Camera:
+    def __init__(self):
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (2592, 1944)
+        self.camera.annotate_background = picamera.Color('black')
+
+    def capture_image(self):
+        now = datetime.now()
+        current_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"{current_time}.jpg"
+        directory = "pictures"
+
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        self.camera.start_preview()
+        self.camera.annotate_text = ""
+        self.camera.capture(os.path.join(directory, filename))
+        self.camera.stop_preview()
+        print(f"Picture saved as {filename} in directory {directory}")
+
+    def __del__(self):
+        self.camera.close()
+
+if __name__ == "__main__":
+    camera = Camera()
+    camera.capture_image()
